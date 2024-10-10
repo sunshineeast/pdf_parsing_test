2c4f61b867b34c380fe4ce5c79c26f71f3503deb
sunshineeast <aksansi03@gmail.com>
2024-10-10 11:03:00 +0530

    Update pdftotext_streamlit_test.py

--- pdftotext_streamlit_test.py
+++ pdftotext_streamlit_test.py
@@ -23,7 +23,8 @@ if st.session_state.uploaded_file:
     st.session_state.doc_parsed = st.session_state.uploaded_file.getvalue()
     # st.session_state.doc_parsed = StringIO(st.session_state.uploaded_file.getvalue().decode("utf-8"))
     # st.session_state.doc_parsed = StringIO(st.session_state.uploaded_file.getvalue())
-    st.write(st.session_state.doc_parsed)
+    with st.expander('Click to View file):
+        st.write(st.session_state.doc_parsed)
 
     # (pdf viewing) works 
     # pdf_viewer(input=st.session_state.doc_parsed, width=700)
@@ -33,8 +34,10 @@ if st.session_state.uploaded_file:
     #     st.session_state.pdf = pdftotext.PDF(f,physical=True)
 
 
-    with open("sample_pdfs/Nonlinear_Optimization_in_R_using_nlopt.pdf", "rb") as f:
-        st.session_state.pdf = pdftotext.PDF(f,physical=True)
+    # with open("sample_pdfs/Nonlinear_Optimization_in_R_using_nlopt.pdf", "rb") as f:
+    #     st.session_state.pdf = pdftotext.PDF(f,physical=True)
+
+    st.session_state.pdf = pdftotext.PDF("sample_pdfs/Nonlinear_Optimization_in_R_using_nlopt.pdf",physical=True)
 
     for st.session_state.page in st.session_state.pdf:
         st.write(st.session_state.page)
